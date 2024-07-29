3d873ea3e2678d8c405a720a75e604c7b1806999
Vashanth <vashanthts@gmail.com>
2024-07-29 11:21:25 +0530

    First Commit

--- src/main/java/com/testing/JunitTask/App.java
+++ src/main/java/com/testing/JunitTask/App.java
@@ -6,10 +6,9 @@ public class App
 	
     private App() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public static int sumOfdigits(int n )
+	public static int sumOfdigits(int n)
     { 
     	int sum=0;
         while(n!=0) {
